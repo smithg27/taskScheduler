7f6010d595274374c5acc9548fd3c367ed741059
Greg Smith <greg.smith@saltermitchell.com>
2018-08-31 13:39:44 -0400

    Added base input modal

--- resources/assets/js/components/TaskInput.vue
+++ resources/assets/js/components/TaskInput.vue
@@ -2,8 +2,39 @@
     <div class="modal" id="task-input">
         <div class="modal-body">
             <div class="form-control">
+                <label for="task-title-input">Title:</label>
                 <div class="input-group">
                     <input id="task-title-input" name="title">
+                </div>
+            </div>
+            <div class="form-control">
+                <label for="task-assignee-input">Assignee:</label>
+                <div class="input-group">
+                    <input id="task-assignee-input" name="assignee">
+                </div>
+            </div>
+            <div class="form-control">
+                <label for="task-description-input">Description:</label>
+                <div class="input-group">
+                    <input id="task-description-input" name="description">
+                </div>
+            </div>
+            <div class="form-control">
+                <label for="task-dueDate-input">Due Date:</label>
+                <div class="input-group">
+                    <input id="task-dueDate-input" name="dueDate">
+                </div>
+            </div>
+            <div class="form-control">
+                <label for="task-type-input">Type:</label>
+                <div class="input-group">
+                    <input id="task-type-input" name="type">
+                </div>
+            </div>
+            <div class="form-control">
+                <label for="task-location-input">Location:</label>
+                <div class="input-group">
+                    <input id="task-location-input" name="location">
                 </div>
             </div>
         </div>
@@ -12,7 +43,11 @@
 
 <script>
     export default {
-        name: "task-input"
+        data() {
+            return {
+                test: 'test',
+            }
+        }
     }
 </script>
 
